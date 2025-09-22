308d126ccb1ba65d24444f127c6035f9d20c4bd2
LGandia <imposiblenormalito@gmail.com>
2025-09-22 11:42:49 +0100

    working urls and views

--- b2b/pub_b2b/urls.py
+++ b2b/pub_b2b/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pub_b2b),
+    path('', views.homepage, name='homepage'),
 ]
