7ac0bd6fd58917b80ee918261996f97db6839b81
SzymonKuczer <szymonkuczer@gmail.com>
2025-09-22 11:46:47 +0100

    added product listing table

--- b2b/pub_b2b/models.py
+++ b2b/pub_b2b/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Retailer(models.Model):
     retailer_account_id = models.AutoField(primary_key=True)  # Auto-incrementing integer PK
+
     name = models.CharField(max_length=255)
     address = models.TextField()
     contact_email = models.EmailField(unique=True)
@@ -20,7 +21,8 @@ class Retailer(models.Model):
 
 
 class  Product(models.Model):
-    Product_ID = models.CharField(max_length=6, primary_key = True)
+    Product_id = models.CharField(max_length=6, primary_key = True)
+
     Product_name = models.CharField(max_length=255)
     Product_Storage_CHOICES = [
         ('Barrel', 'BARREL'),
@@ -35,9 +37,11 @@ class  Product(models.Model):
         ('Ale', 'ALE'),
     ]
     product_type = models.CharField(max_length=20, choices=Product_TYPE_CHOICES)
+    pass
 
 class Supplier(models.Model):
-    supplier_account_id = models.AutoField(primary_key=True)  # Auto-incrementing integer PK
+    supplier_account_id = models.AutoField(primary_key=True) # Auto-incrementing integer PK
+
     name = models.CharField(max_length=255)
     address = models.TextField()
     contact_email = models.EmailField(unique=True)
@@ -51,3 +55,21 @@ class Supplier(models.Model):
     account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPE_CHOICES)
     pass
 
+class Stock(models.Model):
+    stock_id = models.AutoField(primary_key=True)  # Auto-incrementing integer PK
+    retailer_account_id = models.ForeignKey(Retailer, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    beverage_name = models.CharField(max_length=255)
+    pass
+
+class Product_listing(models.Model):
+    p_listing_id = models.AutoField(primary_key=True)  # Auto-incrementing integer PK
+    supplier_account_id = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_available = models.IntegerField()
+    beverage_name = models.CharField(max_length=255)
+    beverage_type = models.CharField(max_length=255)
+    pass
