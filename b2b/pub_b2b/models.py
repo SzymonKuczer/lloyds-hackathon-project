974f52bfde9c74249b469c08d9e2ed72602a7dab
SzymonKuczer <szymonkuczer@gmail.com>
2025-09-22 10:54:56 +0100

    Added primary keys for retailer and supplier tables

--- b2b/pub_b2b/models.py
+++ b2b/pub_b2b/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Retailer(models.Model):
+    retailer_account_id = models.AutoField(primary_key=True)  # Auto-incrementing integer PK
     name = models.CharField(max_length=255)
     address = models.TextField()
     contact_email = models.EmailField(unique=True)
@@ -13,6 +14,21 @@ class Retailer(models.Model):
         ('restaurant', 'Restaurant'),
         ('cafe', 'Cafe'),
         ('bar', 'Bar'),
+    ]
+    account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPE_CHOICES)
+    pass
+
+class Supplier(models.Model):
+    supplier_account_id = models.AutoField(primary_key=True)  # Auto-incrementing integer PK
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    contact_email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    password = models.TextField(max_length=255)
+    ACCOUNT_TYPE_CHOICES = [
+        ('brewery', 'Brewery'),
+        ('bakery', 'Bakery'),
+        ('Butchers', 'Butchers'),
     ]
     account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPE_CHOICES)
     pass
