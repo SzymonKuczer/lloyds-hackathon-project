85b267cbaaf2cf88907feb5548265075f5a21083
LGandia <imposiblenormalito@gmail.com>
2025-09-22 13:25:54 +0100

    update

--- b2b/pub_b2b/urls.py
+++ b2b/pub_b2b/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+path('choose-listing/', views.choose_listing_type, name='choose_listing'),
+    path('create-product/', views.create_plisting, name='create_plisting'),
+    path('create-retail/', views.create_rlisting, name='create_rlisting'),
+    path('products/', views.product_listings, name='product_listing'),
+    path('retail/', views.retail_listings, name='retail_listing')
 ]
--- b2b/pub_b2b/views.py
+++ b2b/pub_b2b/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 
 # Home page view
@@ -24,3 +25,56 @@ def register(request):
 # Optional: main index view
 def pub_b2b(request):
     return render(request, 'homepage.html')
+
+
+# Form request for posts
+from .forms import ProductForm, RetailForm
+from .models import Product_listing, Retail_listing
+
+
+def choose_listing_type(request):
+    if request.method == 'POST':
+        listing_type = request.POST.get('listing_type')
+        if listing_type == 'product':
+            return redirect('create_plisting')
+        elif listing_type == 'retail':
+            return redirect('create_rlisting')
+    return render(request, 'pub_b2b/choose_listing_type.html')
+
+
+@login_required
+def create_plisting(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('product_listing')
+        else:
+            form = ProductForm()
+        return render(request, 'pcreate_post.html', {'form': form})
+
+
+def product_listings(request):
+    products = Product_listing.objects.all().order_by('-id')
+    return render(request, 'product_list.html', {'products': products})
+
+
+@login_required
+def create_rlisting(request):
+    if request.method == 'POST':
+        form = RetailForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('retail_listing')
+        else:
+            form = RetailForm()
+        return render(request, 'rcreate_post.html', {'form': form})
+
+
+def retail_listings(request):
+    products = Retail_listing.objects.all().order_by('-id')
+    return render(request, 'product_list.html', {'products': products})
