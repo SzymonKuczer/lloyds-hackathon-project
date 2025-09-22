a327c9cbb28268ebf38055859f243643f72f09b5
LGandia <imposiblenormalito@gmail.com>
2025-09-22 13:36:40 +0100

    create listing pages

--- b2b/pub_b2b/urls.py
+++ b2b/pub_b2b/urls.py
@@ -3,15 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-<<<<<<< HEAD
-path('choose-listing/', views.choose_listing_type, name='choose_listing'),
+    path('choose-listing/', views.choose_listing_type, name='choose_listing'),
     path('create-product/', views.create_plisting, name='create_plisting'),
     path('create-retail/', views.create_rlisting, name='create_rlisting'),
     path('products/', views.product_listings, name='product_listing'),
-    path('retail/', views.retail_listings, name='retail_listing')
-=======
-
-
+    path('retail/', views.retail_listings, name='retail_listing'),
     path('api/retailers/', views.RetailerListCreate.as_view(), name='retailer-list-create'),
     path('api/retailers/<int:pk>/', views.RetailerDetail.as_view(), name='retailer-detail'),
     path('api/products/', views.ProductListCreate.as_view(), name='product-list-create'),
@@ -24,5 +20,4 @@ path('choose-listing/', views.choose_listing_type, name='choose_listing'),
     path('api/product_listings/<int:pk>/', views.ProductListingDetail.as_view(), name='product-listing-detail'),
     path('api/retail_listings/', views.RetailListingListCreate.as_view(), name='retail-listing-list-create'),
     path('api/retail_listings/<int:pk>/', views.RetailListingDetail.as_view(), name='retail-listing-detail'),
->>>>>>> 3d966c41b21dfa76f1520286a524206726b755d2
 ]
